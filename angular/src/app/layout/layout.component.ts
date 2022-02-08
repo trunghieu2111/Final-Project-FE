3a689c28eb8a128c816ee17a80b3b447cc762f97
Hiếu Trịnh <hieutt27@hotmail.com>
2022-02-08 09:18:20 +0700

    Skip Authen

--- angular/src/app/layout/layout.component.ts
+++ angular/src/app/layout/layout.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '@abp/ng.core';
-import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-layout',
@@ -9,31 +7,8 @@ import { OAuthService } from 'angular-oauth2-oidc';
 })
 export class LayoutComponent implements OnInit {
   isCollapsed = false;
-  get hasLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
-  }
-  constructor(private oAuthService: OAuthService,
-              private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    if (!this.hasLoggedIn) {
-      this.authService.navigateToLogin();
-    }
-  }
-
-  logout(): void {
-    this.authService.logout().subscribe(() => {
-      sessionStorage.clear();
-      localStorage.clear();
-      // this.store.dispatch(
-      //   new Navigate(['/'], null, {
-      //     state: { redirectUrl: this.store.selectSnapshot(RouterState)?.state.url },
-      //   })
-      // );
-    });
-  }
-
-  login() {
-    this.authService.navigateToLogin();
   }
 }
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44399',
+    // issuer: 'https://localhost:44399',
+    issuer: 'https://localhost:44332',
     redirectUri: baseUrl,
     clientId: 'FinalProject_App',
     responseType: 'code',
